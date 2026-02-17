b1b9b11146fa89185cab1ecaa113ba401637e8f0
Quentin-Sutter <153717062+Quentin-Sutter@users.noreply.github.com>
2026-02-17 17:34:52 +0100

    Improve weapon switch guards and button state syncing

--- Assets/Scripts/UI/SwitchWeaponUI.cs
+++ Assets/Scripts/UI/SwitchWeaponUI.cs
@@ -17,13 +17,24 @@ namespace Madbox.UI
             public int weaponIndex;
             public Button button;
             public CooldownFillButton cooldownView;
+
+            [NonSerialized] public ColorBlock defaultColors;
         }
 
         [SerializeField] private WeaponService weaponService;
         [SerializeField] private HeroCombatService heroCombatService;
         [SerializeField] private WeaponButtonBinding[] weaponButtons;
+        [SerializeField] private Color selectedButtonColor = new Color(0.75f, 1f, 0.75f, 1f);
 
-        private int _currentWeaponIndex;
+        private int _currentWeaponIndex = -1;
+        private float _switchCooldownEndTime;
+        private float _attackCooldownEndTime;
+        private int _attackCooldownWeaponIndex = -1;
+        private bool _wasSwitchCooldownActive;
+        private bool _wasAttackCooldownActive;
+
+        private bool IsSwitchCooldownActive => Time.time < _switchCooldownEndTime;
+        private bool IsAttackCooldownActive => Time.time < _attackCooldownEndTime;
 
         private void Awake()
         {
@@ -46,12 +57,15 @@ namespace Madbox.UI
             {
                 weaponService.OnSwitchCooldownStarted += HandleSwitchCooldownStarted;
                 weaponService.OnWeaponChanged += HandleWeaponChanged;
+                _currentWeaponIndex = weaponService.CurrentWeaponIndex;
             }
 
             if (heroCombatService != null)
             {
                 heroCombatService.OnAttackCooldownStarted += HandleAttackCooldownStarted;
             }
+
+            RefreshButtonsState();
         }
 
         private void OnDisable()
@@ -66,6 +80,21 @@ namespace Madbox.UI
             {
                 heroCombatService.OnAttackCooldownStarted -= HandleAttackCooldownStarted;
             }
+        }
+
+        private void Update()
+        {
+            bool isSwitchCooldownActive = IsSwitchCooldownActive;
+            bool isAttackCooldownActive = IsAttackCooldownActive;
+            if (isSwitchCooldownActive == _wasSwitchCooldownActive &&
+                isAttackCooldownActive == _wasAttackCooldownActive)
+            {
+                return;
+            }
+
+            _wasSwitchCooldownActive = isSwitchCooldownActive;
+            _wasAttackCooldownActive = isAttackCooldownActive;
+            RefreshButtonsState();
         }
 
         private void WireButtons()
@@ -83,6 +112,7 @@ namespace Madbox.UI
                     continue;
                 }
 
+                binding.defaultColors = binding.button.colors;
                 int weaponIndex = binding.weaponIndex;
                 binding.button.onClick.RemoveAllListeners();
                 binding.button.onClick.AddListener(() => OnWeaponClicked(weaponIndex));
@@ -97,16 +127,23 @@ namespace Madbox.UI
         private void HandleWeaponChanged(WeaponData weapon, int index)
         {
             _currentWeaponIndex = index;
+            RefreshButtonsState();
         }
 
         private void HandleSwitchCooldownStarted(float duration)
         {
-            StartCooldownForIndex(_currentWeaponIndex, duration);
+            _switchCooldownEndTime = Time.time + Mathf.Max(0f, duration);
+            _wasSwitchCooldownActive = IsSwitchCooldownActive;
+            RefreshButtonsState();
         }
 
         private void HandleAttackCooldownStarted(float duration)
         {
+            _attackCooldownWeaponIndex = _currentWeaponIndex;
+            _attackCooldownEndTime = Time.time + Mathf.Max(0f, duration);
             StartCooldownForIndex(_currentWeaponIndex, duration);
+            _wasAttackCooldownActive = IsAttackCooldownActive;
+            RefreshButtonsState();
         }
 
         private void StartCooldownForIndex(int weaponIndex, float duration)
@@ -118,6 +155,46 @@ namespace Madbox.UI
             }
 
             cooldownView.StartCooldown(duration);
+        }
+
+        private void RefreshButtonsState()
+        {
+            if (weaponButtons == null)
+            {
+                return;
+            }
+
+            bool switchCooldownActive = IsSwitchCooldownActive;
+            bool attackCooldownActive = IsAttackCooldownActive;
+
+            for (int i = 0; i < weaponButtons.Length; i++)
+            {
+                WeaponButtonBinding binding = weaponButtons[i];
+                if (binding == null || binding.button == null)
+                {
+                    continue;
+                }
+
+                bool isSelected = binding.weaponIndex == _currentWeaponIndex;
+                bool isOnAttackCooldown = attackCooldownActive && binding.weaponIndex == _attackCooldownWeaponIndex;
+                bool isInteractable = !isSelected && !switchCooldownActive && !isOnAttackCooldown;
+
+                binding.button.interactable = isInteractable;
+                ApplySelectionVisual(binding, isSelected);
+            }
+        }
+
+        private void ApplySelectionVisual(WeaponButtonBinding binding, bool isSelected)
+        {
+            ColorBlock colors = binding.defaultColors;
+            if (isSelected)
+            {
+                colors.normalColor = selectedButtonColor;
+                colors.highlightedColor = selectedButtonColor;
+                colors.selectedColor = selectedButtonColor;
+            }
+
+            binding.button.colors = colors;
         }
 
         private CooldownFillButton GetCooldownView(int weaponIndex)
